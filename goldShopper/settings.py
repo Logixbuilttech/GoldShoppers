e48902c94934c5534a68950ebd1a781428385a74
demo.logixbuiltinfo <demo.logixbuiltinfo@gmail.com>
2023-03-13 19:44:37 +0530

    added changes

--- goldShopper/settings.py
+++ goldShopper/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = 'django-insecure-%9hlfxbtv%^cllp9@(f=bssyxzh(m(28@gved@4n+6l+rkwtag
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CORS_ALLOWED_ORIGINS = ["*"]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:8000",
+]
 
 
 # Application definition
