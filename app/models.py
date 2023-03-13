ef45a737b53c7b27facc28749ae878935944e818
demo.logixbuiltinfo <demo.logixbuiltinfo@gmail.com>
2023-03-13 14:06:17 +0530

    added changes

--- app/models.py
+++ app/models.py
@@ -1,36 +1,32 @@
 from django.db import models
-from django.contrib.auth.models import User, AbstractUser, Group, Permission
+from django.contrib.auth.models import User, AbstractUser , Group, Permission
 from django.contrib.contenttypes.models import ContentType
 import datetime
 from django.utils.translation import gettext_lazy as _
 from django.core.validators import FileExtensionValidator
 
 
-# class users_role(models.Model):
-#     role = models.CharField(max_length=100)
+class users_role(models.Model):
+    role = models.CharField(max_length=100)
 
-#     def __str__(self):
-#         return str(self.role)
-
+    def __str__(self):
+        return str(self.role)
+    
 # Create your models here.
+class Users(AbstractUser):
 
+    username = models.CharField(max_length = 50, blank = True, null = True, unique = True)
+    email = models.EmailField(_("email address"), unique=True)
+    phone_number = models.CharField(max_length=10, null=True, blank=True)
+    role = models.ForeignKey(users_role, on_delete=models.CASCADE,null=True)
+    description  = models.CharField(max_length=1000, null=True, blank=True)
+    is_staff = models.BooleanField()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
 
-# class Users(AbstractUser):
-
-#     username = models.CharField(
-#         max_length=50, blank=True, null=True, unique=True)
-#     email = models.EmailField(_("email address"), unique=True)
-#     phone_number = models.CharField(max_length=10, null=True, blank=True)
-#     role = models.ForeignKey(users_role, on_delete=models.CASCADE, null=True)
-#     description = models.CharField(max_length=1000, null=True, blank=True)
-#     is_staff = models.BooleanField()
-
-#     USERNAME_FIELD = 'email'
-#     REQUIRED_FIELDS = ['username']
-
-#     def __str__(self):
-#         return str(self.username) or str(self.email)
-
+    def __str__(self):
+        return str(self.username) or str(self.email) 
 
 class Company_Information(models.Model):
 
@@ -46,13 +42,11 @@ class Company_Information(models.Model):
 
 
 class Vendors(models.Model):
-    username = models.CharField(max_length=1000)
-    # username = models.ForeignKey(Users, on_delete=models.CASCADE, null=True)
-    company_name = models.ForeignKey(
-        Company_Information, on_delete=models.CASCADE, null=True)
+
+    username = models.ForeignKey(Users,on_delete=models.CASCADE,null=True)
+    company_name = models.ForeignKey(Company_Information,on_delete=models.CASCADE,null=True)
     address = models.CharField(max_length=1000)
     details = models.CharField(max_length=1000)
     # permission = PermissionError
-
     def __str__(self):
         return str(self.company_name)+" - "+str(self.username)
